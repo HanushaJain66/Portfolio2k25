dce4289912f20f90ea5097a15a83e43ea757c148
Hanusha <u22cs066@coed.svnit.ac.in>
2025-01-26 11:05:22 +0530

    Navbr updated

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -15,6 +15,11 @@ const Navbar = () => {
     sidebar.classList.toggle('show-sidebar');
   };
 
+  const closeSidebar = () => {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.classList.remove('show-sidebar');
+  };
+
   useEffect(() => {
     const handleResize = () => {
       setWindowWidth(window.innerWidth);
@@ -37,13 +42,13 @@ const Navbar = () => {
         {/* Sidebar */}
         <div className='sidebar'>
           <ul>
-          <li><a href="#home" className='nav'>Home</a></li>
-              <li><a href="#about" className='nav'>About</a></li>
-              <li><a href="#education" className='nav'>Education</a></li>
-              <li><a href="#project" className='nav'>Project</a></li>
-              <li><a href="#skills" className='nav'>Skills</a></li>
-              <li><a href="#achivements" className='nav'>Achivements</a></li>
-              <li><a href="#contact" className='nav'>Contact</a></li>
+            <li><a href="#home" className='nav' onClick={closeSidebar}>Home</a></li>
+            <li><a href="#about" className='nav' onClick={closeSidebar}>About</a></li>
+            <li><a href="#education" className='nav' onClick={closeSidebar}>Education</a></li>
+            <li><a href="#project" className='nav' onClick={closeSidebar}>Project</a></li>
+            <li><a href="#skills" className='nav' onClick={closeSidebar}>Skills</a></li>
+            <li><a href="#achivements" className='nav' onClick={closeSidebar}>Achivements</a></li>
+            <li><a href="#contact" className='nav' onClick={closeSidebar}>Contact</a></li>
           </ul>
         </div>
 
